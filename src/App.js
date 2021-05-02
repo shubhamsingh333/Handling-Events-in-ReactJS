3f5c05678f62a2a970ce3336fc7fa406d5d67a47
shubhamsingh333 <shubhamsingh333333@gmail.com>
2021-05-03 01:14:50 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./style.css";
 const App = () => {
   const purple = "#8e44ad";
   const [bg, setBg] = useState(purple);
+  const [name, setName] =useState("click me");
 
   const bgChange = () => {
     // console.log("click")
     let newBg = "#34995e";
     setBg(newBg);
+    setName("ouuch!!");
   };
   return (
     <>
       <div style={{ backgroundColor: bg }}>
-        <button onClick={bgChange}> click me</button>
+        <button onClick={bgChange}> {name}</button>
       </div>
     </>
   );
