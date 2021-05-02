d4ba79b630152ac94cc9daa9a6d1c1fddc37e73a
shubhamsingh333 <shubhamsingh333333@gmail.com>
2021-05-03 01:18:19 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./style.css";
 const App = () => {
   const purple = "#8e44ad";
   const [bg, setBg] = useState(purple);
-  const [name, setName] =useState("click me");
+  const [name, setName] = useState("click me");
 
   const bgChange = () => {
     // console.log("click")
@@ -12,10 +12,17 @@ const App = () => {
     setBg(newBg);
     setName("ouuch!!");
   };
+  const bgBack = () => {
+    setBg(purple);
+    setName("AYYYOO");
+  };
   return (
     <>
       <div style={{ backgroundColor: bg }}>
-        <button onClick={bgChange}> {name}</button>
+        <button onClick={bgChange} onDoubleClick={bgBack}>
+          {" "}
+          {name}
+        </button>
       </div>
     </>
   );
